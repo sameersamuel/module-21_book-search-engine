ef164138f8eb66a4090db7a4486ef18f27590cb1
Sameer B <sameerflyleaf@gmail.com>
2022-11-07 16:35:41 -0800

    typeDefs built

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,21 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBook:[Book]
+}
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+}
+`
